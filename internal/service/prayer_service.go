bc5ea247ff5c3450ff5c79ff15c5dffb736c9d2f
jamaleko <60812791+jamaleko@users.noreply.github.com>
2026-06-01 20:20:08 +0700

    Implement PrayerService with schedule retrieval methods

---
+++ internal/service/prayer_service.go
@@ -0,0 +1,53 @@
+package service
+
+import (
+	"time"
+
+	"prayer-bot/internal/models"
+	"prayer-bot/internal/prayer"
+)
+
+type PrayerService struct{}
+
+func NewPrayerService() *PrayerService {
+	return &PrayerService{}
+}
+
+func (s *PrayerService) GetTodaySchedule(
+	latitude float64,
+	longitude float64,
+) (*models.PrayerSchedule, error) {
+
+	now := time.Now()
+
+	schedule, err := prayer.CalculatePrayerTimes(
+		latitude,
+		longitude,
+		now,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return schedule, nil
+}
+
+func (s *PrayerService) GetScheduleByDate(
+	latitude float64,
+	longitude float64,
+	date time.Time,
+) (*models.PrayerSchedule, error) {
+
+	schedule, err := prayer.CalculatePrayerTimes(
+		latitude,
+		longitude,
+		date,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return schedule, nil
+}
