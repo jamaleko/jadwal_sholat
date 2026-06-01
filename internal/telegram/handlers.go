eec4d0bc5e48e74b02fb56b41c792511f1adf35e
jamaleko <60812791+jamaleko@users.noreply.github.com>
2026-06-01 17:48:49 +0700

    Create handlers.go

---
+++ internal/telegram/handlers.go
@@ -0,0 +1,97 @@
+package telegram
+
+import (
+	"context"
+	"log"
+
+	tgbot "github.com/go-telegram/bot"
+	"github.com/go-telegram/bot/models"
+)
+
+func (b *Bot) registerHandlers() {
+	b.client.RegisterHandler(
+		tgbot.HandlerTypeMessageText,
+		"/start",
+		tgbot.MatchTypeExact,
+		b.handleStart,
+	)
+
+	b.client.RegisterHandler(
+		tgbot.HandlerTypeMessageLocation,
+		"",
+		tgbot.MatchTypeExact,
+		b.handleLocation,
+	)
+}
+
+func (b *Bot) handleStart(
+	ctx context.Context,
+	bot *tgbot.Bot,
+	update *models.Update,
+) {
+	chatID := update.Message.Chat.ID
+
+	msg := `
+Assalamu'alaikum 👋
+
+Selamat datang di Prayer Bot.
+
+Untuk mulai menerima notifikasi jadwal sholat, silakan bagikan lokasi Anda menggunakan tombol di bawah.
+`
+
+	_, err := bot.SendMessage(
+		ctx,
+		&tgbot.SendMessageParams{
+			ChatID: chatID,
+			Text:   msg,
+			ReplyMarkup: &models.ReplyKeyboardMarkup{
+				ResizeKeyboard: true,
+				Keyboard: [][]models.KeyboardButton{
+					{
+						{
+							Text:            "📍 Bagikan Lokasi",
+							RequestLocation: true,
+						},
+					},
+				},
+			},
+		},
+	)
+
+	if err != nil {
+		log.Printf(
+			"failed send start message: %v",
+			err,
+		)
+	}
+}
+
+func (b *Bot) handleLocation(
+	ctx context.Context,
+	bot *tgbot.Bot,
+	update *models.Update,
+) {
+	location := update.Message.Location
+
+	log.Printf(
+		"location received: lat=%f lon=%f",
+		location.Latitude,
+		location.Longitude,
+	)
+
+	_, err := bot.SendMessage(
+		ctx,
+		&tgbot.SendMessageParams{
+			ChatID: update.Message.Chat.ID,
+			Text: "📍 Lokasi berhasil diterima.\n\n" +
+				"Notifikasi jadwal sholat akan segera diaktifkan.",
+		},
+	)
+
+	if err != nil {
+		log.Printf(
+			"failed send location confirmation: %v",
+			err,
+		)
+	}
+}
