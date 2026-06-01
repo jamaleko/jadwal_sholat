de44833f6870f80a517bbea82e5eeabe94be5e1d
jamaleko <60812791+jamaleko@users.noreply.github.com>
2026-06-01 17:43:42 +0700

    Implement Telegram bot structure and initialization

---
+++ internal/telegram/bot.go
@@ -0,0 +1,44 @@
+package telegram
+
+import (
+	"context"
+	"log"
+
+	"github.com/go-telegram/bot"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type Bot struct {
+	client *bot.Bot
+	db     *pgxpool.Pool
+}
+
+func NewBot(
+	token string,
+	db *pgxpool.Pool,
+) (*Bot, error) {
+
+	client, err := bot.New(token)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Bot{
+		client: client,
+		db:     db,
+	}, nil
+}
+
+func (b *Bot) Start() {
+	log.Println("Telegram bot started")
+
+	b.client.Start(context.Background())
+}
+
+func (b *Bot) Client() *bot.Bot {
+	return b.client
+}
+
+func (b *Bot) DB() *pgxpool.Pool {
+	return b.db
+}
